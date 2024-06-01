3e1ee40cbc106bbc03d68f853786eff84fec7fab
pratsha2005 <159262926+pratsha2005@users.noreply.github.com>
2024-06-01 10:48:28 +0000

    Variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 145;
+let accountEmail = "pratsha2005@gmail.com";
+var accountPass = "12121212";
+accountCity = "Meerut"
+
+
+// accountId = 123 (Not Allowed)
+
+
+console.table([accountId, accountEmail, accountPass, accountCity]);
